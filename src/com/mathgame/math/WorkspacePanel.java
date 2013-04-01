88103684304294b3034ea93f6ef8c19696e80c41
Roland Fong <rfblue2@gmail.com>
2013-04-01 15:16:42 -0400

    Slight Image/Layout Change 3
    Added an image for Workspace; wrote code to incorporate image
    *Note - snapping in workspace may need to be moved if titled border is
    removed

--- src/com/mathgame/math/WorkspacePanel.java
+++ src/com/mathgame/math/WorkspacePanel.java
@@ -6,7 +6,12 @@ package com.mathgame.math;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
@@ -15,7 +20,13 @@ import javax.swing.border.TitledBorder;
  * The panel where the cards will be dragged in order to combine and use them
  *
  */
+
+
 public class WorkspacePanel extends JPanel{
+	
+	final String imageFile = "images/Workspace.png";
+	BufferedImage background;
+	
 	public void init()	{
 		this.setLayout(new FlowLayout());
 		TitledBorder workBorder = BorderFactory.createTitledBorder("Workspace");
@@ -26,7 +37,16 @@ public class WorkspacePanel extends JPanel{
 		size.height = 260;
 		setPreferredSize(size);
 		
-		
-		
+		try {
+			background = ImageIO.read(new File(imageFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponents(g);
+		g.drawImage(background, 0, 0, null);
 	}
 }
