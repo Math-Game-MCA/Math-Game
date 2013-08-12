ee0d7b85e4656f1dd602375359b1848a81d8d8e9
Roland Fong <rfblue2@gmail.com>
2013-08-12 17:55:50 -0400

    Menu class created
    Menu class has been added as well as a little code so that David S. can
    start to create the menu

---
+++ src/com/mathgame/math/Menu.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.mathgame.math;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+/**
+ * Class that creates the game Menu
+ */
+public class Menu extends JPanel{
+	
+	//constructor
+	public Menu()	{
+		/*
+		 * For David S.
+		 * Refer to your email for my instructions to you on how to create this menu.
+		 */
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponents(g);
+		//Put your code to "paint" the background here!
+	}
+}
