2af5ed42c67493838dd69fb389ebba6da063b3ba
Ian Lawson <pencam97@gmail.com>
2013-10-16 22:45:53 -0400

    Creation of ValidationBox class
    This class will be used to make validation boxes below number cards

---
+++ src/com/mathgame/math/ValidationBox.java
@@ -0,0 +1,18 @@
+package com.mathgame.math;
+
+import javax.swing.JTextField;
+
+
+
+public class ValidationBox extends JTextField{
+
+	/**
+	 * The purpose of this class is to make a textfield that will be placed below NumberCards
+	 * that will allow a user to enter the number displayed on the card. If the number entered
+	 * matches the number assigned to the card, the box or text will turn green. If they do
+	 * not match, the box or text will turn red. This will be used for testing purposes.
+	 */
+	
+	
+	
+}
