ab7103d092cd836dc1f812a4433099534e674200
rflbue2 <rfblue2@gmail.com>
2014-03-20 18:41:44 -0400

    Added User Class Code creds to Anton

--- src/com/mathgame/network/User.java
+++ src/com/mathgame/network/User.java
@@ -9,4 +9,75 @@ package com.mathgame.network;
  */
 public class User {
 
+	private String name;	
+	private String password;	
+	private int rankvalue;	
+	private int gameswon;	
+	private int gameslost;
+		
+	public User(String username, String pass)
+	{
+		name=username;		
+		password=pass;		
+		rankvalue=1;		
+		gameswon=0;		
+		gameslost=0;		
+	}
+
+	public void GameEnd(Boolean won, int score, User rival)
+	{
+		
+		int modifier=0;
+		
+		if(won==true)
+		{
+			modifier = (rival.getRankvalue()/rankvalue);
+			gameswon++;
+			
+			rankvalue=(rankvalue+(score*modifier));	
+		}
+		if (won==false)
+		{
+			modifier = (rankvalue/rival.getRankvalue());
+			modifier= modifier*-1;
+			gameslost++;
+			
+			rankvalue=(rankvalue+((1/score)*modifier));	
+		}
+		
+		if (rankvalue<1)
+			rankvalue=1;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRankvalue() {
+		return rankvalue;
+	}
+	public void setRankvalue(int rankvalue) {
+		this.rankvalue = rankvalue;
+	}
+	public int getGameswon() {
+		return gameswon;
+	}
+	public void setGameswon(int gameswon) {
+		this.gameswon = gameswon;
+	}
+	public int getGameslost() {
+		return gameslost;
+	}
+	public void setGameslost(int gameslost) {
+		this.gameslost = gameslost;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
