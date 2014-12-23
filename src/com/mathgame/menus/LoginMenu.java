312a55ae0872940b0b0a0dd9254371c4cd239750
ngpaladi <ignore@oneosproject.org>
2014-12-23 16:18:05 -0500

    Login Menu
    I added the login menu with a username and password field and login
    button (password will show up as dots)

--- src/com/mathgame/menus/LoginMenu.java
+++ src/com/mathgame/menus/LoginMenu.java
@@ -6,8 +6,15 @@ package com.mathgame.menus;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+
 
 import com.mathgame.math.MathGame;
 
@@ -28,14 +35,47 @@ public class LoginMenu extends JPanel implements ActionListener {
 	//TODO create JButton: "Log in"
 	
 	//GLOBAL variables (JLabel, JTextField, JButton declarations) go here
+	
 	JLabel title;
-
+	JLabel UsernameLabel;
+	JLabel PasswordLabel;
+	JTextField UsernameField;
+	JPasswordField PasswordField;
+	JButton LoginButton;
+	
 	public LoginMenu()	{
 		//initialize global variables here
 		//TODO setlayout to null
 		//TODO place JLabels, JTextFields, and JButton in this section using "setBounds(x,y,w,l)" method
+
+		
+		setLayout(null);
+		
+		//LoginPanel = new JPanel();
+		
 		title = new JLabel("Epsilon");
-		title.setBounds(MathGame.WIDTH / 2 - 50, 100, 100, 60);
+		title.setBounds(110, 20, 100, 20);
+		this.add(title);
+		
+		UsernameLabel = new JLabel("Username:");
+		UsernameLabel.setBounds(50, 60, 100, 20);
+		this.add(UsernameLabel);
+		
+		PasswordLabel = new JLabel("Password:");
+		PasswordLabel.setBounds(50, 90, 100, 20);
+		this.add(PasswordLabel);
+		
+		UsernameField = new JTextField();
+		UsernameField.setBounds(120, 60, 100, 20);
+		this.add(UsernameField);
+		
+		PasswordField = new JPasswordField();
+		PasswordField.setBounds(120, 90, 100, 20);
+		this.add(PasswordField);
+		
+		LoginButton= new JButton("Log In");
+		LoginButton.setBounds(100, 140, 70, 20);
+		this.add(LoginButton);
 	}
 	
 	@Override
